77e83fd1746069b6279435d1673a70c3b9391f16
= <=>
2024-04-26 23:18:34 +0200

    Commit message

--- src/data/links.ts
+++ src/data/links.ts
@@ -4,4 +4,4 @@ export const telegram = 'https://t.me/frongecoinsol';
 
 export const twitter = 'https://twitter.com/frongcoinsol';
 
-export const contract=''
+export const contract='3QsVJaYKWyQEre5fsAeBuoR8D2LWZK1LC8pTizBG959p'

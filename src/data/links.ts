d4fcd2b7b494e3516ed24edde5303d36fee93d18
= <=>
2024-04-26 23:28:33 +0200

    Commit message

--- src/data/links.ts
+++ src/data/links.ts
@@ -2,6 +2,6 @@ export const dextools = '';
 
 export const telegram = 'https://t.me/frongecoinsol';
 
-export const twitter = 'https://twitter.com/frongcoinsol';
+export const twitter = 'https://twitter.com/frongecoinsol';
 
 export const contract='3QsVJaYKWyQEre5fsAeBuoR8D2LWZK1LC8pTizBG959p'

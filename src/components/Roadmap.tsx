5975e0ac8443d411c182cf407c9f65021b0e8650
= <=>
2024-04-27 12:14:17 +0200

    Commit message

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 const Roadmap = () => {
   return (
-    <div className=" self-center bg-[#81bddb] bg-opacity-35 backdrop-blur-xl rounded-xl 
+    <div
+      className=" 
+    self-center bg-[#81bddb] bg-opacity-35 backdrop-blur-xl rounded-xl 
     
     p-4 w-full
-    lg:p-12 md:w-3/4">
-      <img src="/roadmap.png"/>
-      {/* <img src="/frogLogo.png" className="absolute w-28 rounded-full bottom-20 left-24"/> */}
-      
+    lg:p-12 md:w-3/4"
+    >
+      <img src="/roadmap.png" />
     </div>
   );
 };
--- src/data/links.ts
+++ src/data/links.ts
@@ -1,4 +1,4 @@
-export const dextools = '';
+export const dextools = 'https://www.dextools.io/app/en/solana/pair-explorer/DGQCohGSRxY99KqAA9rcfE49DjFHAbn6xqo9uzBiWj4m?t=1714211589985';
 
 export const telegram = 'https://t.me/frongecoinsol';
 

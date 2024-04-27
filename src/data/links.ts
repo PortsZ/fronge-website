11ce40a62c4af8ec4ab145a622c8bf9afc13bf94
= <=>
2024-04-27 14:14:25 +0200

    Commit message

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,7 +1,7 @@
 export const dextools = '';
 
-export const telegram = 'https://t.me/frongecoinsol';
+export const telegram = 'https://t.me/';
 
-export const twitter = 'https://twitter.com/frongcoinsol';
+export const twitter = 'https://twitter.com/';
 
-export const contract='3QsVJaYKWyQEre5fsAeBuoR8D2LWZK1LC8pTizBG959p'
+export const contract='3QsVJaYKWyQEre5fsAeBuoR8D2LWZK1LC8pTizBG959p' 
